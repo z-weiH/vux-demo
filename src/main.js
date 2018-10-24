7ee5a420a8c3ca7a26e59288dd3214d5e80d66e4
zengwei <18279047775@163.com>
2018-10-24 10:54:05 +0800

    vconsole 也显示在 build环境中

--- src/main.js
+++ src/main.js
@@ -31,10 +31,10 @@ import cookie from 'js-cookie'
 Vue.prototype.$cookie = cookie;
 
 // 引入 vconsole
-if(process.env.NODE_ENV === 'development') {
+//if(process.env.NODE_ENV === 'development') {
   let VConsole = require('vconsole');
   const vConsole = new VConsole();
-}
+//}
 
 
 /* eslint-disable no-new */
